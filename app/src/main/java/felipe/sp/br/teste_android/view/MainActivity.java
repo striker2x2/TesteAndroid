b78f2b223c45196f4b86f6c7936b1a90f826b477
Felipe frazão <felipe11xx@hotmail.com>
2018-06-08 19:29:05 -0300

    criação dos xmls base para as telas de contato e investimento

---
+++ app/src/main/java/felipe/sp/br/teste_android/view/MainActivity.java
@@ -0,0 +1,21 @@
+package felipe.sp.br.teste_android.view;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+import felipe.sp.br.teste_android.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+
+}
